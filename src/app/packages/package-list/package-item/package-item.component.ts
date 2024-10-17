95463e21bf419b2b1058ab192b0d771d7352402b
Niishimuraa <henriqueyudi123@gmail.com>
2024-10-17 09:52:46 -0300

    Feito componentes iniciais para o projeto

---
+++ src/app/packages/package-list/package-item/package-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-package-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './package-item.component.html',
+  styleUrl: './package-item.component.css'
+})
+export class PackageItemComponent {
+
+}
