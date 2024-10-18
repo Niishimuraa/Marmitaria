439b98ed44e7bc1f97d6571fbe3ba667f160a280
Niishimuraa <henriqueyudi123@gmail.com>
2024-10-18 08:16:03 -0300

    Componente da marmita

--- src/app/packages/package-list/package-list.component.ts
+++ src/app/packages/package-list/package-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PackageItemComponent } from "./package-item/package-item.component";
+import { Package } from './package.model';
 
 @Component({
   selector: 'app-package-list',
@@ -9,7 +10,9 @@ import { PackageItemComponent } from "./package-item/package-item.component";
   styleUrl: './package-list.component.css'
 })
 export class PackageListComponent implements OnInit{
-  packages = [];
+  packages: Package[] = [
+    new Package('Churrasco', 'Churrasco de picanha', 'https://www.ogastronomo.com.br/upload/2051998133-churrasco-uma-paixao-nacional.jpg')
+  ];
   
   constructor() {}
 
